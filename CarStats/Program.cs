1697ead15043519f0db74fef8c7f59436b0121d5
Heron Y L Uriu <heronuriu5@gmail.com>
2025-03-21 18:04:12 +0000

    CarStats

---
+++ CarStats/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Bogus;
+using Spectre.Console;
+
+namespace CarStats
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Randomizer.Seed = new Random(int.Parse(args[0]));
+           
+            for each (args)
+            {
+                Faker faker = new Faker();
+                faker number = new int
+                BarChart bc = new BarChart(faker);
+                Console.Writeline($"{faker} {bc} {args}")
+            }
+
+            Faker faker = new Faker();
+            Faker 
+            BarChart bc = new BarChart();
+        }
+
+        public static class FakeData()
+        {
+            .RuleFor(c => c.Name, f => f.Bogus.DataSets.CompanyName)
+            .RuleFor()
+        }
+    }
+}
